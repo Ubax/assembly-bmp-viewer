74efaaad923f4695b40c642af6511084daf8bd71
Ubax <32908614+Ubax@users.noreply.github.com>
2019-04-05 18:12:04 +0200

    256 bitmap

--- files.asm
+++ files.asm
@@ -17,7 +17,7 @@ WRONG_HEADER_MSG	db	"Wrong bmp header$"
 TOO_BIG_IMAGE_MSG	db	"The image is too big. Max size: 320x200$"
 FILE_OPENING_MSG	db	"File opening error. Probably file doesn't exist$"
 
-fileName        	db	"abc.bmp$";100 dup(0)
+fileName        	db	"test1.bmp$";100 dup(0)
 
 file				dw	?
 bmp_header			db	15 dup(0)
@@ -99,55 +99,76 @@ set_pallete:
 	mov byte ptr cs:[g], 0
 	mov byte ptr cs:[b], 0
 	
-	mov si, 0
-	mov cx, 8
-	set_palette_r_loop:
-		mov word ptr cs:[r_iterator], cx
-		mov cx, 4
-		set_palette_g_loop:
-			mov word ptr cs:[g_iterator], cx
-			mov cx, 8
-			set_palette_b_loop:
-				mov word ptr cs:[b_iterator], cx
-				mov ax, si
-				out dx, al
-				inc dx
-				mov al, byte ptr cs:[r]
-				out dx, al
-				mov al, byte ptr cs:[g]
-				out dx, al
-				mov al, byte ptr cs:[b]
-				out dx, al
-				dec dx
-				inc si
-				
-				add byte ptr cs:[b], 9
-				mov ax, word ptr cs:[b_iterator]
-				mov cx, ax
-				loop set_palette_b_loop
-			add byte ptr cs:[g], 21
-			mov ax, word ptr cs:[g_iterator]
-			mov cx, ax
-			loop set_palette_g_loop
-		add byte ptr cs:[r], 9
-		mov ax, word ptr cs:[r_iterator]
-		mov cx, ax
-		loop set_palette_r_loop
-	mov al,255
-	out dx, al
-	inc dx
-	mov al, 63
-	out dx, al
-	mov al, 63
-	out dx, al
-	mov al, 63
-	out dx, al
-	dec dx
-	
+	xor si, si
+	set_pallete_loop:
+		mov	dx,offset bmp_bufor
+		mov	bx,word ptr ds:[file]	
+		mov	cx,4  ;bgra
+		mov	ah,3fh
+		int	21h
+		mov dx, 3c8h
+		mov ax, si
+		out dx, al
+		inc dx
+		mov cx,2
+		mov al, byte ptr ds:[bmp_bufor+2]
+		shr al, cl
+		out dx, al
+		mov al, byte ptr ds:[bmp_bufor+1]
+		shr al, cl
+		out dx, al
+		mov al, byte ptr ds:[bmp_bufor]
+		shr al, cl
+		out dx, al
+		dec dx
+		inc si
+		cmp si, 256
+		jl set_pallete_loop
+		
 show_bmp:
-	mov al, 7
+	mov al, 225
 	call clear_display
-	call show_bitmap
+	mov cx, 0
+	y_loop:
+		mov bx, 0
+		x_loop:
+			push bx
+			push cx
+			mov	dx,offset bmp_bufor
+			mov	bx,word ptr ds:[file]	
+			mov	cx,1  ;bgra
+			mov	ah,3fh
+			int	21h
+			pop cx
+			pop bx
+			mov	word ptr cs:[point_x], bx
+			mov	word ptr cs:[point_y], cx
+			push bx
+			push cx
+			mov al, byte ptr ds:[bmp_bufor]	
+			mov	byte ptr cs:[point_k], al
+			call draw_point
+			pop cx
+			pop bx
+			inc bx
+			mov ax, word ptr ds:[bmp_width]
+			cmp bx, ax
+			jl x_loop
+		inc cx
+		mov ax, word ptr ds:[bmp_height]
+		cmp cx, ax
+		jl y_loop
+	;mov ax, 0a000h
+	;mov ds, ax
+	;mov dx, 64000-320
+	;mov cx,320
+;image:
+	;mov	ah,3fh
+	;int	21h
+	;jnc wrong_header
+	
+	;sub dx, 320
+	;jnc image
 key_loop:
 	in al, 60h		;get key scancode
 	cmp al, ESC_KEY		
@@ -185,8 +206,13 @@ b_iterator		dw	0
 r				db	0
 g				db	0
 b				db	0
-offset_x		db	0
-offset_y		db 	0
+a				db	0
+offset_x		dw	0
+offset_y		dw 	0
+zoom_in			db 	1
+zoom_out		db	1
+cur_x			dw	0
+cur_y			dw 	0
 
 ;-------------------------------------------------------
 ;----------------------- POINT -------------------------
@@ -252,79 +278,8 @@ clear_display:
 	ret
 
 show_bitmap:	
-	;-- si - cols iterator
-	;-- di - rows iterator
-	mov al, 255;
-	call clear_display
 	
-	; pushf
-	; push cx
-	; push dx
-	; push di
-	; push si
-	; pushf
-	mov	bx,word ptr ds:[file]
-	mov cx, 0
-	mov dx,	54
-	mov ax, 4200h
-	int 21h
-	mov cx, 100
-	bmp_load_pixel_map_rows_loop_beg:
-		push cx
-		mov di,cx
-		dec di
-		mov cx, 100
-		xor si,si
-		bmp_load_pixel_map_cols_loop_beg:
-			push cx
-			mov	dx,offset bmp_bufor
-			mov	bx,word ptr ds:[file]	
-			mov	cx,3  ;ilosc bajtow do czytania
-			mov	ah,3fh
-			int	21h
-					
-			;---- X
-			mov	word ptr cs:[point_x], si
-			
-			mov	word ptr cs:[point_y], di
-			
-			;---- COLOR
-			xor bx,bx
-			xor dx,dx
-			mov bl, byte ptr ds:[bmp_bufor]
-			mov cx, 5
-			shr bx, cl
-			shl bx, cl
-			add dx, bx
-			mov bl, byte ptr ds:[bmp_bufor+1]
-			mov cx, 6
-			shr bx, cl
-			mov cx, 3
-			shl bx, cl
-			add dx, bx
-			mov bl, byte ptr ds:[bmp_bufor+2]
-			mov cx, 5
-			shr bx, cl
-			add dx, bx
-			
-			mov byte ptr cs:[point_k], dl
-			call draw_point
-			
-			inc si
-			mov ax, word ptr ds:[bmp_width]
-			cmp si, ax
-			pop cx
-			jge bmp_load_pixel_map_cols_loop_end
-			loop bmp_load_pixel_map_cols_loop_beg
-		bmp_load_pixel_map_cols_loop_end:
-		pop cx
-		loop bmp_load_pixel_map_rows_loop_beg
-	; popf
-	; push si
-	; push di
-	; push dx
-	; push cx
-	; popf		
+	
 	ret
 
 include io.asm
